238f901d8cfaf773a00bf3d7296cedfcaab0b2fd
bananaWeb <a89.volkov@gmail.com>
2018-02-05 20:56:23 +0300

    rocket science

--- script.js
+++ script.js
@@ -1,16 +1,66 @@
 $(document).ready(function(){
 
-	 $('.js-Speak').click(function(){
-		 var inputText = $('.js-text').val();
-		 showBubble($('.js-text1').val() + $('.js-text2').val());
-	 });
+
+
+ 	$('.js-plus').click(function(){
+ 		showBubble(Number(getFirstWord()) + Number(getSecondWord()));
+ 	});
+ 	$('.js-minus').click(function(){
+ 		showBubble(Number(getFirstWord()) - Number(getSecondWord()));
+ 	});
+ 	$('.js-multiply').click(function(){
+ 		showBubble(Number(getFirstWord()) * Number(getSecondWord()));
+ 	});
+ 	$('.js-div').click(function(){
+ 		 if (checkInputData().isNumber) {
+		 showBubble(Number(getFirstWord()) / Number(getSecondWord()));
+		} else {
+			if (checkInputData().isEmpty) {
+				showBubble('Пустоши какие-то');
+			} else {
+				showBubble(getFirstWord()+' and '+getSecondWord());
+			}
+		}
+ 	});
+
+ 	// ====================================== //
+ 	// ====================================== //
+
+    function checkInputData(){
+    	if ($.isNumeric(getFirstWord()) && $.isNumeric(getSecondWord())) {
+    		return{
+    			isNumber: true
+    		};
+
+    	}else if (getFirstWord() === '' && getSecondWord() === '') {
+    		return {
+    			isNumber: false,
+    			isEmpty: true
+    		};
+    	}else {
+    		return{
+    			isNumber: false,
+    			isEmpty: false
+    		};
+    	}
+    }
+
+
+
+ 	function getFirstWord(){
+ 		return $('.js-text1').val();
+ 	}
+
+ 	function getSecondWord(){
+ 		return $('.js-text2').val();
+ 	}
 
 	 function showBubble(text) {
 	 	$('.bubble').addClass('-visible');
 	 	$('.bubble').text(text);
 	 	setTimeout(function() {
 	 		$('.bubble').removeClass('-visible')
-	 	}, 2000);
+	 	}, 5000);
 	 }
 
 });
