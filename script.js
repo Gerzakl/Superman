bba0fbb9df1d65939aabbf060d4c385365d7d352
bananaWeb <a89.volkov@gmail.com>
2018-02-08 21:06:06 +0300

    classwork 8.02

--- script.js
+++ script.js
@@ -1,30 +1,95 @@
 $(document).ready(function(){
 
+    let Hero = {
+        name: 'Silvana',
+        age: 67,
+        job: 'DeathKnight',
+
+        params: {
+            height: 160,
+            weight: 'unabled',
+            gender: 'Female',
+            race: 'Blood elf'
+        },
+
+        powers: {
+            mainPower: 'Soulshadow',
+            secondaryPower: 'Deatharrow'
+        }    
+    };
+
+    $('.js-object').click(function(){
+        showBubbleWithBlock('<div class="bubble-test">Hero name</div>');
+    });
+
+    let movies = ['banana', 'apple', 'door', 'orange'];
+
+    $('.js-array').click(function(){
+        movies.push(getSecondWord());
+        showBubble(movies);
+    });
+
+
+    for (let i = 0; i < movies.length; i++) {
+        $('.array-panel').append('<div class="test-item">' + movies[i] + '</div>');
+    };
+
+
+
+
+
+
+
+
+
+
 
 
  	$('.js-plus').click(function(){
- 		showBubble(Number(getFirstWord()) + Number(getSecondWord()));
+ 		 if (checkInputData().isNumber) {
+         showBubble(Number(getFirstWord()) + Number(getSecondWord()));
+        } else {
+            showStringData();
+        }
  	});
  	$('.js-minus').click(function(){
- 		showBubble(Number(getFirstWord()) - Number(getSecondWord()));
+ 		 if (checkInputData().isNumber) {
+         showBubble(Number(getFirstWord()) - Number(getSecondWord()));
+        } else {
+            showStringData();
+        }
  	});
  	$('.js-multiply').click(function(){
- 		showBubble(Number(getFirstWord()) * Number(getSecondWord()));
+ 		 if (checkInputData().isNumber) {
+         showBubble(Number(getFirstWord()) * Number(getSecondWord()));
+        } else {
+            showStringData();
+        }
  	});
  	$('.js-div').click(function(){
- 		 if (checkInputData().isNumber) {
-		 showBubble(Number(getFirstWord()) / Number(getSecondWord()));
-		} else {
-			if (checkInputData().isEmpty) {
-				showBubble('Пустоши какие-то');
-			} else {
-				showBubble(getFirstWord()+' and '+getSecondWord());
-			}
+        if (checkInputData().isNumber) {
+            if(getSecondWord() =='0'){
+                showBubble('Так вымерли динозавры!');
+            } else {
+                showBubble(getFirstWord() / getSecondWord());
+            };
+        }  else {
+		 showStringData();
 		}
  	});
 
  	// ====================================== //
  	// ====================================== //
+
+
+    function showStringData(){
+        if (checkInputData().isEmpty) {
+                showBubble('Пустоши какие-то');
+            } else {
+                showBubble(getFirstWord()+' and '+getSecondWord());
+            }
+    }
+
 
     function checkInputData(){
     	if ($.isNumeric(getFirstWord()) && $.isNumeric(getSecondWord())) {
@@ -32,13 +97,13 @@ $(document).ready(function(){
     			isNumber: true
     		};
 
-    	}else if (getFirstWord() === '' && getSecondWord() === '') {
+    	} else if (getFirstWord() === '' && getSecondWord() === '') {
     		return {
     			isNumber: false,
     			isEmpty: true
     		};
-    	}else {
-    		return{
+    	} else {
+    		return {
     			isNumber: false,
     			isEmpty: false
     		};
@@ -63,4 +128,12 @@ $(document).ready(function(){
 	 	}, 5000);
 	 }
 
+     function showBubbleWithBlock(block) {
+        $('.bubble').addClass('-visible');
+        $('.bubble').append(block);
+        setTimeout(function() {
+            $('.bubble').removeClass('-visible')
+        }, 5000);
+     }
 });
+
